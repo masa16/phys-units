e37d914a33e8b3d66521b8921cd88e73a98671df
Masahiro TANAKA <masa16.tanaka@gmail.com>
2013-04-30 20:14:49 +0900

    add doc

--- lib/phys/units/mixin.rb
+++ lib/phys/units/mixin.rb
@@ -1,5 +1,11 @@
 require "phys/units"
 
+# ActiveSupport-like mix-in to Numeric.
+# This is activated by:
+#     require 'phys/units/mixin'
+#
+# However, this kind of global change will cause unexpected problems.
+
 class Numeric
   alias method_missing_orig method_missing
   def method_missing(method, *args, &block)
